e7b0bd18d452c51ac7c1993c68bbace83e256a9d
Scott Windsor <swindsor@gmail.com>
2011-10-01 14:32:02 -0700

    Fix guid reservation

--- app/controllers/match_controller.rb
+++ app/controllers/match_controller.rb
@@ -16,18 +16,19 @@ class MatchController < ApplicationController
       if $redis.scard("lobby") > 1
         $redis.srem "lobby", id
         oppenent_id = $redis.spop "lobby"
+        guid = Guid.new.to_s
+        $redis.hset "channels", id, guid
+        $redis.hset "channels", oppenent_id, guid
         $redis.hset "games", id, oppenent_id
         $redis.hset "games", oppenent_id, id
-        guid = Guid.new
-        $redis.hset "channels", id, guid.to_s
-        $redis.hset "channels", oppenent_id, guid.to_s
-        render :json => {:matched => true, :id => oppenent_id, :guid => guid.to_s}
+        render :json => {:matched => true, :id => oppenent_id, :guid => guid}
       else
         render :json => {:matched => false}
       end
     elsif $redis.hexists "games", id
       oppenent_id = $redis.hget "games", id
       guid = $redis.hget "channels", id
+      logger.debug "guid = #{guid}"
       render :json => {:matched => true, :id => oppenent_id, :guid => guid}
     else
       render :json => {:matched => false}
