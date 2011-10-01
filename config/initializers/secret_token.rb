425978aa9062d4319512dde6eef0ea3e0ff6bbd9
Scott Windsor <swindsor@gmail.com>
2011-10-01 10:50:03 -0700

    Initial generated app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MathDeathMatchService::Application.config.secret_token = '2d90de78672a319a0129dbe4f973dd1a103c6dde270ab58fc4c36e9b2e072f42a3eaf4730daf05b4af50b3d003de089b31678c2345c3df0998f7135c3a31df80'
