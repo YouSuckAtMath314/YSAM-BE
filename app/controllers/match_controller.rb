f6c5cf15f1e75325c00ced053f6cad9a429b988a
Scott Windsor <swindsor@gmail.com>
2011-10-01 15:42:45 -0700

    Store custom user params

--- app/controllers/match_controller.rb
+++ app/controllers/match_controller.rb
@@ -3,32 +3,38 @@ class MatchController < ApplicationController
   def join
     id = params[:id]
     oppenent_id = $redis.hdel "games", id
+    user_params = params.reject{|k| ["id", "controller", "action"].include?(k) }
+    logger.debug "user_params = #{user_params.inspect}"
     $redis.hdel "games", oppenent_id
     $redis.hdel "channels", id
     $redis.hdel "channels", oppenent_id
     $redis.sadd "lobby", id
-    render :json => {:id => params[:id]}
+    $redis.hset "users", id, user_params.to_json
+    render :json => {:id => params[:id], :user => user_params}
   end
 
   def status
     id = params[:id]
+    user = $redis.hget("users", id)
+    if user
+      user_params = ActiveSupport::JSON.decode($redis.hget("users", id))
+    end
     if $redis.sismember "lobby", id
       if $redis.scard("lobby") > 1
         $redis.srem "lobby", id
         oppenent_id = $redis.spop "lobby"
-        guid = Guid.new.to_s
-        $redis.hset "channels", id, guid
-        $redis.hset "channels", oppenent_id, guid
         $redis.hset "games", id, oppenent_id
         $redis.hset "games", oppenent_id, id
-        render :json => {:matched => true, :id => oppenent_id, :guid => guid}
+        guid = Guid.new.to_s
+        $redis.hset "channels", id, guid.to_s
+        $redis.hset "channels", oppenent_id, guid.to_s
+        render :json => {:matched => true, :id => oppenent_id, :guid => guid.to_s, :user => user_params}
       else
         render :json => {:matched => false}
       end
     elsif $redis.hexists "games", id
       oppenent_id = $redis.hget "games", id
       guid = $redis.hget "channels", id
-      logger.debug "guid = #{guid}"
       render :json => {:matched => true, :id => oppenent_id, :guid => guid}
     else
       render :json => {:matched => false}
